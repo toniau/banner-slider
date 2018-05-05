bb818b05871c8d96fb4dc24469b54113d8824de8
toniau <toni.au94@gmail.com>
2018-05-04 21:38:20 -0400

    Slide on hover implemented

--- interaction.js
+++ interaction.js
@@ -1,7 +1,22 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
+  /*$('.owl-carousel').owlCarousel({
     items: 1,
     loop: true,
     margin: 10
+  }); */
+  
+  $('.right-panel').hover(
+    function(){
+      $(this).css('z-index',4);
+    }, function(){
+      setTimeout(changeIndex, 500);
   });
+  
+  $('.left-panel').click(function(){
+    
+  })
 });
+
+function changeIndex() {
+  $('.right-panel').css('z-index',1);
+}
