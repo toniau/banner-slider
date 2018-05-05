2d9178c4accf7c351b6ad1b89611c2bf771997db
toniau <toni.au94@gmail.com>
2018-05-05 15:14:12 -0400

    Fixing transition when you hover from right to left panel, implementing click interaction

--- interaction.js
+++ interaction.js
@@ -7,16 +7,36 @@ $(document).ready(function(){
   
   $('.right-panel').hover(
     function(){
-      $(this).css('z-index',4);
+      $(this).css('z-index', 4);
+      $(this).addClass('expand-left');
     }, function(){
-      setTimeout(changeIndex, 500);
+      setTimeout(changeIndex, 100)
+      $(this).removeClass('expand-left');
+      $('.right-panel').promise().done(function(){
+
+      if($('.left-panel').is(':hover')) {
+        $('.left-panel').addClass('expand-right2');
+        console.log('EXECUTED if(L_PAN hover)');
+      }
+    });
   });
   
+  $('.left-panel').hover(
+    function(){
+      $(this).removeClass('expand-right2');
+      $(this).addClass('expand-right');
+      console.log("I just executed normal L-PAN hover")
+    }, function() {
+      $(this).removeClass('expand-right');
+    }
+  );
+  
   $('.left-panel').click(function(){
-    
+    // remember to look at gifffer for play/pause GIF lib
+    $(this).css('background-image', 'url(imgs/hk-stag2.jpg)')
   })
 });
 
 function changeIndex() {
-  $('.right-panel').css('z-index',1);
+  $('.right-panel').css('z-index', 1);
 }
