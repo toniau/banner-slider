8f67a0544d1786a6244ba5ed470a535f889e1d0d
toniau <toni.au94@gmail.com>
2018-05-05 21:01:38 -0400

    Added click function, edit SV gif

--- interaction.js
+++ interaction.js
@@ -1,64 +1,84 @@
 // IIFE - Immediately Invoked Function Expression
-(function($, window, document) {
+(function ($, window, document) {
 
   // The $ is now locally scoped 
 
- // Listen for the jQuery ready event on the document
- $(document).ready(function() {
+  // Listen for the jQuery ready event on the document
+  $(document).ready(function () {
 
-   // The DOM is ready!
+    // The DOM is ready!
 
-   /*$('.owl-carousel').owlCarousel({
-      `items: 1,
-      loop: true,
-      margin: 10
-    }); */
-   
-   let left_panel  = $('.left-panel'),
-       right_panel = $('.right-panel'),
-       panel_blurb = $('.panel-blurb');
+    /*$('.owl-carousel').owlCarousel({
+       `items: 1,
+       loop: true,
+       margin: 10
+     }); */
 
-   // Panel: Hover Functions */
-   left_panel.hover(
-      function(){
+    let left_panel  = $('.left-panel'),
+        right_panel = $('.right-panel'),
+        panel_blurb = $('.panel-blurb');
+    
+    /* ---------
+    ** LEFT Panel Functions
+    ** Handles hovering and clicking actions of LEFT banner panel only.
+    ** To edit animations, see CSS classes tagged as 'left-panel'.
+    ** --------- */
+    left_panel.on({
+      
+      'mouseenter': function() {
         $(this).removeClass('left-panel__expand2');
         $(this).addClass('left-panel__expand');
         console.log("EXECUTED normal L_PAN hover");
-      }, function() {
+      },
+      
+      'mouseleave': function() {
         $(this).removeClass('left-panel__expand left-panel__full');
         $('.panel-blurb').hide();
+      },
+      
+      'click': function() {
+        $(this).addClass('left-panel__full')
+        $('.left-panel .panel-blurb').show()
       }
-    );
-   
-    right_panel.hover(
-      function(){
+    });
+    
+    /* ---------
+    ** RIGHT Panel Functions
+    ** Handles hovering and clicking actions of RIGHT banner panel only.
+    ** To edit animations, see CSS classes tagged as 'right-panel'.
+    ** --------- */
+    right_panel.on({
+      
+      'mouseenter': function() {
         $(this).css('z-index', 4);
         $(this).addClass('right-panel__expand');
-      }, 
-      function(){
+      },
+      
+      'mouseleave': function() {
         setTimeout(changeIndex, 100)
         $(this).removeClass('right-panel__expand right-panel__full');
         $('.panel-blurb').hide();
-        right_panel.promise().done(function(){
+        
+        right_panel.promise().done(function () {
 
-        if(left_panel.is(':hover')) {
-          left_panel.addClass('left-panel__expand2');
-          console.log('EXECUTED if(L_PAN hover)');
-        }
-      });
+          if (left_panel.is(':hover')) {
+            left_panel.addClass('left-panel__expand2');
+            console.log('EXECUTED if(L_PAN hover)');
+          }
+        });
+      },
+      
+      'click': function() {
+        $(this).addClass('right-panel__full')
+        $('.right-panel .panel-blurb').show()
+      }
     });
 
-   left_panel.click(function(){
-      $(this).addClass('left-panel__full')
-      $('.left-panel .panel-blurb').show()
-    })
+    
+  // End doc ready  
+  });
 
-   right_panel.click(function(){
-      $(this).addClass('right-panel__full')
-      $('.right-panel .panel-blurb').show()
-    })
- });
-
+  
   function changeIndex() {
     $('.right-panel').css('z-index', 1);
   }
